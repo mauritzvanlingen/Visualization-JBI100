313c1a534faec43e76d8b46602275843a6762522
RobertSmoldersTUe <l.j.p.smolders@student.tue.nl>
2024-01-17 14:41:05 +0100

    2.0

--- dashframework-main/app-robert.py
+++ dashframework-main/app-robert.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     pc = ParallelCharts(
         color_team_1=[200,0,50],
         color_team_2=[0, 200, 50],
-        data_source_path="FIFA DataSet/Data/" # set path to data folder of FIFA dataset
+        # data_source_path="FIFA DataSet/Data/" # set path to data folder of FIFA dataset
     )
     pc.read_files()
     pc.preprocess_data()
@@ -29,34 +29,42 @@ if __name__ == '__main__':
             html.Div(
                 id="right-column",
                 className="nine columns",
-                children=pc.figures(['data_set_main'], 'Argentina', 'France')[1] #default
+                children=[pc.figure([
+                    "shots_on_target_per90", 
+                    "shots_on_target_pct",
+                    "goals_per_shot_on_target",
+                    "into_penalty_area",
+                    "pct_touches_att_pen_area",
+                    "xg_per90",
+                    "average_shot_distance",
+                    ], 'Argentina', 'France')]
             ),
         ],
     )
 
-    # If graph is selected, then graph is added and the variables of the graphs are added as an option to explanation
-    @app.callback(
-        [Output('explanation_selection', 'options'), 
-         Output('right-column', 'children')],
-        [Input('select-team-1', 'value'),
-         Input('select-team-2', 'value'),
-        Input('graph-dropdown', 'value')]
-    )
-    def update_graphs(team_1, team_2, selected_graphs):
-        if isinstance(selected_graphs, list):
-            return pc.figures(selected_graphs, team_1, team_2)
-        else:
-            return pc.figures([selected_graphs], team_1, team_2)
+    # # # If graph is selected, then graph is added and the variables of the graphs are added as an option to explanation
+    # @app.callback(
+    #     [Output('explanation_selection', 'options'), 
+    #      Output('right-column', 'children')],
+    #     [Input('select-team-1', 'value'),
+    #      Input('select-team-2', 'value'),
+    #     Input('attribute-dropdown', 'value')]
+    # )
+    # def update_graphs(team_1, team_2, selected_graphs):
+    #     if isinstance(selected_graphs, list):
+    #         return pc.figure(selected_graphs, team_1, team_2)
+    #     else:
+    #         return pc.figure([selected_graphs], team_1, team_2)
 
-    # Display explanation of selected variable
-    @app.callback(
-        [Output('explanation', 'children')],
-        [Input('explanation_selection', 'value')]
-    )
-    def update_explanation(variable):
-        if variable:
-            return [f"This is the explanation for variable: {variable}"]
-        else:
-            return ["Choose a variable for which to show the explanation here."]
+    # # Display explanation of selected variable
+    # @app.callback(
+    #     [Output('explanation', 'children')],
+    #     [Input('explanation_selection', 'value')]
+    # )
+    # def update_explanation(variable):
+    #     if variable:
+    #         return [f"This is the explanation for variable: {variable}"]
+    #     else:
+    #         return ["Choose a variable for which to show the explanation here."]
 
     app.run_server(debug=False, dev_tools_ui=False)
